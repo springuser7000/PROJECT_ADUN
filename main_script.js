ec5e7a4749c2588939ed199949f061444e772f3c
springuser7000 <springuser700@gmail.com>
2019-07-15 08:23:38 +0900

    asfd

---
+++ main_script.js
@@ -0,0 +1,20 @@
+import "https://cdn.jsdelivr.net/npm/vue"
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: '안녕하세요 Vue!'
+    }
+  })
+
+
+var app4 = new Vue({
+el: '#app-4',
+data: {
+    todos: [
+    { text: 'JavaScript 배우기' },
+    { text: 'Vue 배우기' },
+    { text: '무언가 멋진 것을 만들기' }
+    ]
+}
+})
